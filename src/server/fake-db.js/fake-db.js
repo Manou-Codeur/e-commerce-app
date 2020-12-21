d69d8b4b73142c6de4f3940da3887e67625e07d3
salim06000 <ayachesalim06000@gmail.com>
2020-12-21 21:34:03 +0100

    start the app

---
+++ src/server/fake-db.js/fake-db.js
@@ -0,0 +1,10 @@
+export const shoes = {
+  airJordanElevation: {
+    blue: {
+      1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d7e2664c-32ec-4f85-a882-93a0f6c66059/chaussure-de-basketball-jordan-react-elevation-pour-3MNcdW.jpg",
+    },
+    green: {},
+    red: {},
+  },
+};
+const clothes = {};
