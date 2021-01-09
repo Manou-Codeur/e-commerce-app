46f4b76fa3a6a4c19ac240edf73c8d174a3279f7
Manou-Codeur <Manou-Codeur@outlook.com>
2021-01-09 23:57:24 +0100

    done with <Recommend />

--- src/components/main/home/trending/trendsWrapper/trend/trend.jsx
+++ src/components/main/home/trending/trendsWrapper/trend/trend.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import HistoryContext from "./../../../../../../context/historyContext";
 
 import "./trend.scss";
 
-const Trend = ({ imgUrl, big }) => {
+const Trend = ({ data: { imgUrl, name }, big }) => {
+  const { history } = useContext(HistoryContext);
+
   return (
-    <div className={big ? "trend--big" : "trend"}>
+    <div
+      className={big ? "trend--big" : "trend"}
+      onClick={() => history.push(`/product/${name}`)}
+    >
       <div className={big ? "trend--big__img-wrapper" : "trend__img-wrapper"}>
         <img src={imgUrl} alt="shoe image" />
       </div>
 
-      <div className={big ? "trend--big__name" : "trend__name"}>
-        Nike Zoom X
-      </div>
+      <div className={big ? "trend--big__name" : "trend__name"}>{name}</div>
     </div>
   );
 };
--- src/components/main/home/trending/trendsWrapper/trendsWrapper.jsx
+++ src/components/main/home/trending/trendsWrapper/trendsWrapper.jsx
@@ -3,20 +3,25 @@ import React from "react";
 import Trend from "./trend/trend";
 
 import "./trendsWrapper.scss";
-import AirJordanXI from "../../../../../assets/img/trending-products/jordan2.jpg";
-import Lacoste from "../../../../../assets/img/trending-products/lacoste.jpg";
+import nike from "../../../../../assets/img/trending-products/jordan2.jpg";
+import LacosteSneakers from "../../../../../assets/img/trending-products/lacoste.jpg";
 import NikeZoom from "../../../../../assets/img/trending-products/nikeZoom.jpg";
 
 const TrendsWrapper = () => {
   return (
     <div className="trends-wrapper">
       <div className="trends-wrapper__big">
-        <Trend imgUrl={AirJordanXI} big={true} />
+        <Trend
+          data={{ imgUrl: nike, name: "Nike React Infinity Run Flyknit 2" }}
+          big={true}
+        />
       </div>
 
       <div className="trends-wrapper__small">
-        <Trend imgUrl={Lacoste} />
-        <Trend imgUrl={NikeZoom} />
+        <Trend
+          data={{ imgUrl: LacosteSneakers, name: "Lacoste Sneakers Run" }}
+        />
+        <Trend data={{ imgUrl: NikeZoom, name: "Nike Zoom X" }} />
       </div>
     </div>
   );
