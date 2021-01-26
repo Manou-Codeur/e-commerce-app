fb91e2c4f56929037eaeed3a755d41dfd08af09f
Manou-Codeur <Manou-Codeur@outlook.com>
2021-01-26 23:08:27 +0100

    quick css refactors

--- src/components/reuseable/recommend/productsSlider/product/product.jsx
+++ src/components/reuseable/recommend/productsSlider/product/product.jsx
@@ -7,11 +7,14 @@ import "./product.scss";
 const Product = ({ data: { name, type, genre, imgUrl, color } }) => {
   const { history } = useContext(HistoryContext);
 
+  const goToProductPage = () => {
+    history.push(`/product/${type}@${genre}@${name}@${color}`);
+    //this is when we are already in the "product" router we need to scroll the page to the top automatically
+    window.scrollTo({ top: 0 });
+  };
+
   return (
-    <div
-      className="product"
-      onClick={() => history.push(`/product/${type}@${genre}@${name}@${color}`)}
-    >
+    <div className="product" onClick={goToProductPage}>
       <div className="product__img-wrapper">
         <img src={imgUrl} alt="product img" />
       </div>
