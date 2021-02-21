c7521134f3aaff3b8528147d0008c4104a3a5278
khaled-codeur <manou-codeur@outlook.com>
2021-02-21 16:46:33 +0100

    done with the static version of the app, still need some code refactor for best code quality

--- src/components/main/notFound/notFound.jsx
+++ src/components/main/notFound/notFound.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+import HistoryContext from "./../../../context/historyContext";
+import NavBar from "./../../reuseable/navBar/navBar";
+
 import "./notFound.scss";
 
-const NotFound = () => {
-  return <h1>Not found</h1>;
+const NotFound = ({ history }) => {
+  return (
+    <div className="not-found">
+      <HistoryContext.Provider value={{ history }}>
+        <NavBar />
+      </HistoryContext.Provider>
+
+      <div className="not-found__main">
+        <h1>Oops... Page Not Found!</h1>
+
+        <button onClick={() => history.push("/")}>Go Home</button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
