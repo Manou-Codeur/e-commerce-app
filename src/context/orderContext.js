14540cf981418b03c6893753e29f4a1273ab3be1
khaled-codeur <manou-codeur@outlook.com>
2021-12-31 02:11:42 +0100

    test

--- src/context/orderContext.js
+++ src/context/orderContext.js
@@ -1,4 +1,4 @@
-// This react context will be used by the <Order /> and it's childs
+// This react context will be used by the <Order /> and its childs
 // and it will contain the function that let the order component go to next steps
 
 import { createContext } from "react";
